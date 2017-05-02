f8edff12bc60c5cbd2726cedc4076a8c9e096e5c
Batu Ozsaracoglu <batuoz@yahoo.com>
2017-05-02 15:26:48 -0700

    Prints out counts of unique charecters
    Correctly finds each unique character in the file and prints out its frequency

--- huffman.cpp
+++ huffman.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include "BinaryTree.h"
 #include "BinaryHeap.h"
 
@@ -17,7 +18,29 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
-    cout << argv[1] << endl;
+    //int array which stores the frequencies of each character. The ASCII code of each character is the index/key in the array
+    int* freq = new int[128];
+    
+    //Open file to be read
+    ifstream file(argv[1]);
+    
+    //Current character being read
+    char curr;
+    
+    //Read each character in the file
+    while( file >> curr){
+        //Increase count of current character
+        freq[(int)curr]++;
+    }
+    
+    //Print frequencies of each character
+    for(int i = 0; i < 128; i++){
+        
+        if(freq[i] != 0){
+          cout << (char) i << " : " << freq[i] << endl;
+        }
+    }
+    
     
     return 0;
 }
